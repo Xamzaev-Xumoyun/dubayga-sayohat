85bdffb7e5c4991e605c8c3c98ee120940aef1fc
Xamzaev-Xumoyun <xamzaevxumoyun49@gmail.com>
2022-02-20 15:53:24 +0500

    Intial commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+
+var a = Number(500);
+var b = Number(250);
+var c = Number(120);
+
+var a = (a * 10900);
+var b = (b * 10900);
+var c = (c * 12273);
+
+
+var second = ("Kechirasiz mablag'ingiz yetarli emas")
+
+var first = ("Oq yo'l");
+
+var promt =  prompt("pullingizni kiriting"  );
+
+var over = Number((a + b + c));
+ 
+ 
+
+if( promt >= over)
+{
+console.log(first)}
+else
+{
+console.log(second)}
+
+
+
+
+
+
+
+
+
+
+
+
